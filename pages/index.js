058509825d1516a1d7612a72078963313c4d4c0b
Braulio Truylio <braulio.truylio@gmail.com>
2023-06-13 22:14:49 +0000

    ajustado tamanho

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <><h1>Aeeee - Dá-lhe AUGUSTO! </h1><iframe width="598" height="1063" src="https://www.youtube.com/embed/BtFGftLA-l8" title="E ae" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></>
+    return <><h1>Aeeee - Dá-lhe AUGUSTO! </h1><iframe width="300" height="530" src="https://www.youtube.com/embed/BtFGftLA-l8" title="E ae" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></>
 }
 
 export default Home;
